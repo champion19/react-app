07bffcb597cdc459a4b47f449f4000155cea453a
Emmanuel Londoño :) <emmalondogo@gmail.com>
2022-06-04 10:15:50 -0500

    add index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import  ReactDOM  from 'react-dom';
+import{createRoot} from 'react-dom/client';
 //import PrimeraApp from './PrimeraApp';
 import CounterApp from './CounterApp';
 
@@ -7,8 +7,5 @@ import './index.css';
 
 //const saludo=<h1>Hey world</h1>;//variable que crea un elemento html
 const divRoot=document.querySelector('#app');
-
-
-
-ReactDOM.render(<CounterApp value={100}/>,divRoot);
-
+const root=createRoot(divRoot);
+root.render(<CounterApp value={10}/>,divRoot);
